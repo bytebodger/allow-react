f8b4b9892a25d73ee0ae87d7ae9d7f3fdf534f49
Adam Nathaniel Davis <adam.davis@semanticbits.com>
2021-04-06 22:45:09 -0400

    removed dependency on @toolz/allow

--- src/allow.js
+++ src/allow.js
@@ -1,104 +1,288 @@
-import { allow as core } from '@toolz/allow';
 import React from 'react';
 import { isARegularObject } from '@toolz/is-a-regular-object-react';
+import { sessionStorageIsAvailable } from '@toolz/session-storage-is-available';
 
 const Allow = () => {
+   let allowNull;
+   let currentFailureBehavior;
+   const failureBehavior = {
+      IGNORE: 'ignore',
+      THROW: 'throw',
+      WARN: 'warn',
+   };
+   if (sessionStorageIsAvailable()) {
+      const savedAllowNull = sessionStorage.getItem('allow.allowNull');
+      allowNull = savedAllowNull === 'true';
+      currentFailureBehavior = sessionStorage.getItem('allow.failureBehavior') || failureBehavior.THROW;
+   } else {
+      allowNull = false;
+      currentFailureBehavior = failureBehavior.THROW;
+   }
+   let onFailure = () => {
+      //
+   };
+   
+   const aBoolean = value => {
+      if (typeof value !== 'boolean')
+         return fail(value, 'is not a Boolean');
+      return allow;
+   };
+   
+   const aFunction = value => {
+      if (typeof value !== 'function')
+         return fail(value, 'is not a function');
+      return allow;
+   };
+   
+   const anArray = (value, minLength = 0, maxLength = Number.MAX_SAFE_INTEGER) => {
+      anInteger(minLength, is.not.negative).anInteger(maxLength, is.not.negative);
+      if (!Array.isArray(value))
+         return fail(value, 'is not an array');
+      checkLength(value, minLength, maxLength);
+      return allow;
+   };
+   
+   const anArrayOfArrays = (value, minLength = 0, maxLength = Number.MAX_SAFE_INTEGER) => {
+      anArray(value).anInteger(minLength, is.not.negative).anInteger(maxLength, is.not.negative);
+      if (allowNull && value === null)
+         return allow;
+      value.forEach(item => anArray(item));
+      checkLength(value, minLength, maxLength);
+      return allow;
+   };
+   
+   const anArrayOfInstances = (value, modelObject, minLength = 0, maxLength = Number.MAX_SAFE_INTEGER) => {
+      anArray(value).anObject(modelObject).anInteger(minLength, is.not.negative).anInteger(maxLength, is.not.negative);
+      if (allowNull && value === null)
+         return allow;
+      value.forEach(item => anInstanceOf(item, modelObject));
+      checkLength(value, minLength, maxLength);
+      return allow;
+   };
+   
+   const anArrayOfIntegers = (value, minLength = 0, maxLength = Number.MAX_SAFE_INTEGER) => {
+      anArray(value).anInteger(minLength, is.not.negative).anInteger(maxLength, is.not.negative);
+      if (allowNull && value === null)
+         return allow;
+      value.forEach(item => anInteger(item));
+      checkLength(value, minLength, maxLength);
+      return allow;
+   };
+   
+   const anArrayOfNumbers = (value, minLength = 0, maxLength = Number.MAX_SAFE_INTEGER) => {
+      anArray(value).anInteger(minLength, is.not.negative).anInteger(maxLength, is.not.negative);
+      if (allowNull && value === null)
+         return allow;
+      value.forEach(item => aNumber(item));
+      checkLength(value, minLength, maxLength);
+      return allow;
+   };
+   
    const anArrayOfObjects = (value, minLength = 0, maxLength = Number.MAX_SAFE_INTEGER) => {
-      core.anArray(value).anInteger(minLength, is.not.negative).anInteger(maxLength, is.not.negative);
-      if (core.getAllowNull() && value === null)
+      anArray(value).anInteger(minLength, is.not.negative).anInteger(maxLength, is.not.negative);
+      if (getAllowNull() && value === null)
          return allow;
       let reactElementFound = false;
       value.forEach(item => {
          if (reactElementFound)
             return;
          if (React.isValidElement(item))
-            return core.fail(item, 'is a React element');
-         core.anObject(item);
+            return fail(item, 'is a React element');
+         anObject(item);
       });
-      core.checkLength(value, minLength, maxLength);
+      checkLength(value, minLength, maxLength);
       return allow;
    };
    
    const anArrayOfReactElements = (value, minLength = 0, maxLength = Number.MAX_SAFE_INTEGER) => {
-      core.anArray(value).anInteger(minLength, is.not.negative).anInteger(maxLength, is.not.negative);
-      if (core.getAllowNull() && value === null)
+      anArray(value).anInteger(minLength, is.not.negative).anInteger(maxLength, is.not.negative);
+      if (getAllowNull() && value === null)
          return allow;
       value.forEach(item => aReactElement(item));
-      core.checkLength(value, minLength, maxLength);
+      checkLength(value, minLength, maxLength);
+      return allow;
+   };
+   
+   const anArrayOfStrings = (value, minLength = 0, maxLength = Number.MAX_SAFE_INTEGER) => {
+      anArray(value).anInteger(minLength, is.not.negative).anInteger(maxLength, is.not.negative);
+      if (allowNull && value === null)
+         return allow;
+      value.forEach(item => aString(item));
+      checkLength(value, minLength, maxLength);
+      return allow;
+   };
+   
+   const anInstanceOf = (suppliedObject, modelObject) => {
+      anObject(suppliedObject).anObject(modelObject);
+      if (allowNull && suppliedObject === null)
+         return allow;
+      const modelKeys = Object.keys(modelObject);
+      let aKeyIsMissing = false;
+      modelKeys.forEach(modelKey => {
+         if (!suppliedObject.hasOwnProperty(modelKey))
+            aKeyIsMissing = true;
+         else {
+            const suppliedValue = suppliedObject[modelKey];
+            const modelValue = modelObject[modelKey];
+            const isSuppliedValueAnObject = isARegularObject(suppliedValue);
+            const isSuppliedValueAnArray = Array.isArray(suppliedValue);
+            const isModelValueAnObject = isARegularObject(modelValue);
+            const isModelValueAnArray = Array.isArray(modelValue);
+            if (isSuppliedValueAnObject !== isModelValueAnObject || isSuppliedValueAnArray !== isModelValueAnArray)
+               fail(suppliedObject, 'does not match the model object');
+            else if (isModelValueAnObject)
+               anInstanceOf(suppliedValue, modelValue);
+         }
+      });
+      if (aKeyIsMissing)
+         fail(suppliedObject, 'is missing a required key');
+      return allow;
+   };
+   
+   const anInteger = (value, minValue = Number.MIN_SAFE_INTEGER, maxValue = Number.MAX_SAFE_INTEGER) => {
+      if (!Number.isInteger(value))
+         return fail(value, 'is not an integer');
+      checkRange(value, minValue, maxValue);
       return allow;
    };
    
    const anObject = (value, minNumberOfKeys = 0, maxNumberOfKeys = Number.MAX_SAFE_INTEGER) => {
-      core.anInteger(minNumberOfKeys, is.not.negative).anInteger(maxNumberOfKeys, is.not.negative);
+      anInteger(minNumberOfKeys, is.not.negative).anInteger(maxNumberOfKeys, is.not.negative);
       if (React.isValidElement(value))
-         return core.fail(value, 'is a React element');
+         return fail(value, 'is a React element');
       if (!isARegularObject(value))
-         return core.fail(value, 'is not an object');
-      core.checkLength(Object.keys(value), minNumberOfKeys, maxNumberOfKeys);
+         return fail(value, 'is not an object');
+      checkLength(Object.keys(value), minNumberOfKeys, maxNumberOfKeys);
+      return allow;
+   };
+   
+   const aNumber = (value, minValue = Number.MIN_SAFE_INTEGER, maxValue = Number.MAX_SAFE_INTEGER) => {
+      if (typeof value !== 'number')
+         return fail(value, 'is not a number');
+      checkRange(value, minValue, maxValue);
       return allow;
    };
    
    const aReactElement = value => {
       if (!React.isValidElement(value))
-         return core.fail(value, 'is not a React element');
+         return fail(value, 'is not a React element');
       return allow;
    };
    
-   const failureBehavior = {
-      IGNORE: 'ignore',
-      THROW: 'throw',
-      WARN: 'warn',
+   const aString = (value, minLength = 0, maxLength = Number.MAX_SAFE_INTEGER) => {
+      anInteger(minLength, is.not.negative).anInteger(maxLength, is.not.negative);
+      if (typeof value !== 'string')
+         return fail(value, 'is not a string');
+      checkLength(value, minLength, maxLength);
+      return allow;
    };
+   
+   const checkLength = (value, minLength = 0, maxLength = Number.MAX_SAFE_INTEGER) => {
+      anInteger(minLength, 0).anInteger(maxLength, 0);
+      if (value.length < minLength)
+         return fail(value, 'is too short');
+      if (value.length > maxLength)
+         return fail(value, 'is too long');
+      return true;
+   };
+   
+   const checkRange = (value = 0, minValue = Number.MIN_SAFE_INTEGER, maxValue = Number.MAX_SAFE_INTEGER) => {
+      if (value < minValue)
+         return fail(value, 'is too small');
+      if (value > maxValue)
+         return fail(value, 'is too large');
+      return true;
+   };
+   
+   const fail = (value, message) => {
+      if (allowNull && value === null)
+         return allow;
+      onFailure(value, message);
+      if (currentFailureBehavior === 'ignore')
+         return allow;
+      if (currentFailureBehavior === 'warn') {
+         console.warn(value);
+         console.warn(`[${value.toString()}] ${message}`);
+         return allow;
+      }
+      console.error(value);
+      throw new Error(`[${value.toString()}] ${message}`);
+   };
+   
+   const getAllowNull = () => allowNull;
+   
+   const getFailureBehavior = () => currentFailureBehavior;
+   
+   const getOnFailure = () => onFailure;
    
    const is = {not: {negative: 0}};
    
    const oneOf = (value, allowedValues) => {
-      if (core.getAllowNull() && value === null)
+      if (getAllowNull() && value === null)
          return allow;
       if (isARegularObject(value) || Array.isArray(value) || typeof value === 'function' || React.isValidElement(value)) {
-         core.fail(value, 'cannot be an object, array, function, or a React element');
+         fail(value, 'cannot be an object, array, function, or a React element');
          return allow;
       }
       if (React.isValidElement(value) || typeof allowedValues !== 'object' || allowedValues === null) {
-         core.fail(allowedValues, 'allowedValues must be supplied in an object or an array');
+         fail(allowedValues, 'allowedValues must be supplied in an object or an array');
          return allow;
       }
       if (Array.isArray(allowedValues)) {
          if (!allowedValues.some(allowedValue => value === allowedValue))
-            return core.fail(value, 'is not an allowed value');
+            return fail(value, 'is not an allowed value');
          return allow;
       }
       const entries = Object.entries(allowedValues);
       if (!entries.some(entry => entry[1] === value))
-         return core.fail(value, 'is not an allowed value');
+         return fail(value, 'is not an allowed value');
       return allow;
    };
    
+   const setAllowNull = newAllowNull => {
+      aBoolean(newAllowNull);
+      if (sessionStorageIsAvailable())
+         sessionStorage.setItem('allow.allowNull', newAllowNull.toString());
+      allowNull = newAllowNull;
+   };
+   
+   const setFailureBehavior = behavior => {
+      oneOf(behavior, failureBehavior);
+      if (sessionStorageIsAvailable())
+         sessionStorage.setItem('allow.failureBehavior', behavior);
+      currentFailureBehavior = behavior;
+   };
+   
+   const setOnFailure = onFailureFunction => {
+      aFunction(onFailureFunction);
+      onFailure = onFailureFunction;
+   };
+   
    return {
-      aBoolean: core.aBoolean,
-      aFunction: core.aFunction,
-      anArray: core.anArray,
-      anArrayOfArrays: core.anArrayOfArrays,
-      anArrayOfInstances: core.anArrayOfInstances,
-      anArrayOfIntegers: core.anArrayOfIntegers,
-      anArrayOfNumbers: core.anArrayOfNumbers,
+      aBoolean: aBoolean,
+      aFunction: aFunction,
+      anArray: anArray,
+      anArrayOfArrays: anArrayOfArrays,
+      anArrayOfInstances: anArrayOfInstances,
+      anArrayOfIntegers: anArrayOfIntegers,
+      anArrayOfNumbers: anArrayOfNumbers,
       anArrayOfObjects,
       anArrayOfReactElements,
-      anArrayOfStrings: core.anArrayOfStrings,
-      anInstanceOf: core.anInstanceOf,
-      anInteger: core.anInteger,
+      anArrayOfStrings: anArrayOfStrings,
+      anInstanceOf: anInstanceOf,
+      anInteger: anInteger,
       anObject,
-      aNumber: core.aNumber,
+      aNumber: aNumber,
       aReactElement,
-      aString: core.aString,
+      aString: aString,
       failureBehavior,
-      getAllowNull: core.getAllowNull,
-      getFailureBehavior: core.getFailureBehavior,
-      getOnFailure: core.getOnFailure,
+      getAllowNull: getAllowNull,
+      getFailureBehavior: getFailureBehavior,
+      getOnFailure: getOnFailure,
       oneOf,
-      setAllowNull: core.setAllowNull,
-      setOnFailure: core.setOnFailure,
-      setFailureBehavior: core.setFailureBehavior,
+      setAllowNull: setAllowNull,
+      setOnFailure: setOnFailure,
+      setFailureBehavior: setFailureBehavior,
    };
 };
 
